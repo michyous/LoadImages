02b93a4ef1896cfd95644443014f53c1705d80c0
Michael Youssef <michyous@gmail.com>
2018-03-23 05:57:24 +0000

    Fixed reg_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,7 +17,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
     def select_plan
       unless (params[:plan] == '1' || params[:plan] == '2')
-      flash[:notice] = "Please select a membership plan to sign up."
-      redirect_to root_url
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
     end
 end
