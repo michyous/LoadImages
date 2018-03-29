92b9325e93456c5d8c4da1b3a4247973a669a422
Michael Youssef <michyous@gmail.com>
2018-03-29 02:00:02 +0000

    Added the query parameters for sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   # GET request for / which is our home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+    @once_plan = Plan.find(3)
   end
   
   def about
